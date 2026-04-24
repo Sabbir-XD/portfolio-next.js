89b9d86d53a78b1b5cc89cb1a8f849a7e51d5d3f
Sabbir-XD <hossain.sabbir.dev@gmail.com>
2026-04-25 01:19:47 +0600

    remove side space full width set

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900`}
       >
         <Navbar />
-        <main className="w-11/12 mx-auto">{children}</main>
+        <main className="w-12/12 mx-auto">{children}</main>
         <Footer />
       </body>
     </html>
